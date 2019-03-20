155fe88bff1292693e4aee1fce8b835d8e2dffb8
Roy Wu <roywu@catchplay.com>
2019-03-20 16:04:59 +0800

    test echo

--- controller/linehook.go
+++ controller/linehook.go
@@ -2,12 +2,30 @@ package controller
 
 import (
 	"fmt"
-
 	"roybot/config"
+
+	"github.com/line/line-bot-sdk-go/linebot"
 
 	"github.com/gin-gonic/gin"
 )
 
+//Callback : Setup https server for receiving request from LINE platform
 func Callback(c *gin.Context) {
-	fmt.Printf("Hi %+v", config.Conf)
+	// fmt.Printf("Hi %+v", config.Conf)
+	events, err := config.Bot.ParseRequest(c.Request)
+	if err != nil {
+		fmt.Printf("Request got error %+v", err)
+		return
+	}
+
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				if _, err = config.Bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+					fmt.Printf("ReplyMessage got error %+v", err)
+				}
+			}
+		}
+	}
 }
